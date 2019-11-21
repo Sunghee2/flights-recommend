0d798baa67732617acbdd4cd934070e5d34e4a6c
minsour <sparkm0128@gmail.com>
2019-11-22 05:29:46 +0900

    CLN: CityItem active 할 때 밑줄 적용

--- client/src/components/CityList/CityItem.jsx
+++ client/src/components/CityList/CityItem.jsx
@@ -3,18 +3,17 @@ import Styled from 'styled-components';
 import PropTypes from 'prop-types';
 import { CityContext } from './../../stores';
 
-const ItemBody = Styled.li`
+const CityItem = ({ name, airport, active }) => {
+  console.log('CityItem');
+  const { city, setCity } = useContext(CityContext);
+  const ItemBody = Styled.li`
   margin: 0em 0.6em;
   padding: 0.6em 1.2em;
-  border: solid 0.5px white;
-  border-radius: 3em;
+  ${active && `border-bottom: 1px solid white;`}
+  font-size: 1.1em;
   color: white;
   cursor: pointer;
 `;
-
-const CityItem = ({ name, airport }) => {
-  console.log('CityItem');
-  const { city, setCity } = useContext(CityContext);
 
   const changeCity = useCallback(() => {
     if (city.airport !== airport) setCity({ name, airport });
@@ -26,6 +25,7 @@ const CityItem = ({ name, airport }) => {
 CityItem.propTypes = {
   name: PropTypes.string.isRequired,
   airport: PropTypes.string.isRequired,
+  active: PropTypes.bool.isRequired,
 };
 
 export default CityItem;
