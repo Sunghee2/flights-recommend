b77beb516e9d9ae7a2c20414531c278c5a509138
Eunwoo <dmsdn960@gmail.com>
2019-11-21 19:50:37 +0900

    ENH: 티켓 리스트 쿼리 작성

--- client/src/components/TicketList/index.jsx
+++ client/src/components/TicketList/index.jsx
@@ -1,9 +1,15 @@
 import React, { useContext, useEffect } from 'react';
 import styled from 'styled-components';
+import { useQuery } from '@apollo/react-hooks';
+import GET_TICKETS from './query';
 import { CityContext } from './../../stores';
 
 const TicketList = _ => {
   const { cityList, setCityList } = useContext(CityContext);
+  const { loading, error, data } = useQuery(GET_TICKETS);
+
+  if (loading) return <div>loading...</div>;
+  if (error) return <div>error</div>;
 
   return (
     <div>
