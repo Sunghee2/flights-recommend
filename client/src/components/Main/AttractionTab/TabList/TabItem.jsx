64f3735fd828c784af917a2f1be373c8ffda7b27
Eunwoo <dmsdn960@gmail.com>
2019-11-22 00:59:32 +0900

    CLN: 컨텍스트 변경 호출 위치 수정 #23

--- client/src/components/Main/AttractionTab/TabList/TabItem.jsx
+++ client/src/components/Main/AttractionTab/TabList/TabItem.jsx
@@ -1,15 +1,26 @@
 import React, { useContext, useCallback } from 'react';
 import Styled from 'styled-components';
 import PropTypes from 'prop-types';
+import { TotalContext } from './../../../../stores';
 
 const TabBody = Styled.li`
 `;
 
 const TabItem = props => {
+  const { setHotel, setTour } = useContext(TotalContext);
   console.log('TabItem');
   const changeTab = useCallback(() => {
     props.setCurrentTab(props.name);
-    props.setTotal({});
+    switch (props.name) {
+      case 'hotel':
+        setTour({});
+        break;
+      case 'tour':
+        setHotel({});
+        break;
+      default:
+        break;
+    }
   });
 
   return <TabBody onClick={changeTab}>{props.name}</TabBody>;
@@ -19,7 +30,6 @@ TabItem.propTypes = {
   name: PropTypes.string.isRequired,
   active: PropTypes.bool.isRequired,
   setCurrentTab: PropTypes.func.isRequired,
-  setTotal: PropTypes.func.isRequired,
 };
 
 export default TabItem;
--- client/src/components/Main/AttractionTab/TabList/index.jsx
+++ client/src/components/Main/AttractionTab/TabList/index.jsx
@@ -2,7 +2,6 @@ import React, { useContext } from 'react';
 import Styled from 'styled-components';
 import TabItem from './TabItem';
 import PropTypes from 'prop-types';
-import { TotalContext } from './../../../../stores';
 
 const ListBody = Styled.ul`
 `;
@@ -10,22 +9,14 @@ const ListBody = Styled.ul`
 const tabList = ['hotel', 'tour'];
 
 const TabList = props => {
-  const { setHotel, setTour } = useContext(TotalContext);
   console.log('TabList');
   console.log(props.currentTab);
   return (
     <ListBody>
       {tabList.map((tab, index) => {
         const active = tab === props.currentTab;
-        const setTotal = tab === 'hotel' ? setHotel : setTour;
         return (
-          <TabItem
-            key={index}
-            name={tab}
-            active={active}
-            setCurrentTab={props.setCurrentTab}
-            setTotal={setTotal}
-          />
+          <TabItem key={index} name={tab} active={active} setCurrentTab={props.setCurrentTab} />
         );
       })}
     </ListBody>
