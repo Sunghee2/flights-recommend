37c19f018471168f5e64500d26690ab4a06371c3
minsour <sparkm0128@gmail.com>
2019-11-22 02:35:08 +0900

    ENH: Plan에서 days.map으로 DayPlan 렌더 #33

--- client/src/components/Main/Plan/index.jsx
+++ client/src/components/Main/Plan/index.jsx
@@ -1,12 +1,17 @@
 import React, { useEffect, useContext } from 'react';
 import { IterContext } from '../../../stores';
+import DayPlan from './DayPlan';
 
 const Plan = _ => {
   console.log('plan');
   const { days } = useContext(IterContext);
+  console.log(days);
   return (
     <div>
-      <div>일정 {days.length}</div>
+      <div>일정</div>
+      {days.map((day, index) => {
+        <DayPlan index={index} hotel={day.hotel} tours={day.tours} />;
+      })}
     </div>
   );
 };
