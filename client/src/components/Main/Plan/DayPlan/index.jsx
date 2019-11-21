4b1e97cba38405ae8b28c7ac9e0fa7ea77a38fdd
Eunwoo <dmsdn960@gmail.com>
2019-11-22 01:19:35 +0900

    ENH: 호텔, 투어 아이템 컴포넌트 재사용 #27

--- client/src/components/Main/Plan/DayPlan/index.jsx
+++ client/src/components/Main/Plan/DayPlan/index.jsx
@@ -1,16 +1,40 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import HotelItem from './../../AttractionTab/AttractionList/HotelItem';
+import TourItem from './../../AttractionTab/AttractionList/TourItem';
 
-import HotelPlan from './HotelPlan';
-import TourPlan from './TourPlan';
-
-const DayPlan = _ => {
-  const Tours = [].map(() => <TourPlan />);
+const DayPlan = ({ index, hotel, tours }) => {
+  const Tours = tours.map(({ _id, name, price, rank, image }) => (
+    <TourItem key={_id} name={name} price={price} rank={rank} image={image} />
+  ));
   return (
     <div>
-      <HotelPlan />
+      {hotel && (
+        <HotelItem name={hotel.name} price={hotel.price} rate={hotel.rate} image={hotel.image} />
+      )}
       <div>{Tours}</div>
     </div>
   );
+};
+
+DayPlan.propTypes = {
+  index: PropTypes.number.isRequired,
+  hotel: PropTypes.shape({
+    _id: PropTypes.string,
+    name: PropTypes.string,
+    price: PropTypes.number,
+    rate: PropTypes.number,
+    image: PropTypes.string,
+  }).isRequired,
+  tours: PropTypes.arrayOf(
+    PropTypes.shape({
+      _id: PropTypes.string,
+      name: PropTypes.string,
+      price: PropTypes.number,
+      rank: PropTypes.number,
+      image: PropTypes.string,
+    }),
+  ).isRequired,
 };
 
 export default DayPlan;
