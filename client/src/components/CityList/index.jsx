f982719239bcb7b07a8252be4ea519813c8d727a
devsoopark <spark0128@dgu.ac.kr>
2019-11-21 20:51:28 +0900

    ENH: CityList 추가

---
+++ client/src/components/CityList/index.jsx
@@ -0,0 +1,29 @@
+import React, { useContext } from 'react';
+import Styled from 'styled-components';
+import CityItem from './CityItem';
+import { CityContext } from './../../stores';
+
+const ListBody = Styled.ul`
+`;
+const ItemName = Styled.div`
+
+`;
+const ItemDate = Styled.div`
+  
+`;
+
+const CityList = props => {
+  console.log('CityList');
+  const { city, cityList } = useContext(CityContext);
+  console.log(cityList);
+  console.log(city);
+  return (
+    <ListBody>
+      {cityList.map((city, index) => (
+        <CityItem key={index} name={city.name} airport={city.airport} />
+      ))}
+    </ListBody>
+  );
+};
+
+export default CityList;
--- client/src/routers/HomePage/index.jsx
+++ client/src/routers/HomePage/index.jsx
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom';
 import Title from '../../components/Title';
 import { CityStore } from '../../stores';
 import TicketList from '../../components/TicketList';
+import CityList from '../../components/CityList';
 
 const HomePage = ({ match, history, location }) => {
   return (
@@ -10,6 +11,7 @@ const HomePage = ({ match, history, location }) => {
       <CityStore>
         <Link to="/plan/111">Plan</Link>
         <TicketList />
+        <CityList />
         <Title label="Hello World!" />
       </CityStore>
     </div>
