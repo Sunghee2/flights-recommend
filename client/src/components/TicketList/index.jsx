c4f82c759d7e840a58e765e2942a8787b36ac477
Eunwoo <dmsdn960@gmail.com>
2019-11-21 20:16:22 +0900

    ENH: 티켓 리스트 뷰

--- client/src/components/TicketList/index.jsx
+++ client/src/components/TicketList/index.jsx
@@ -1,21 +1,51 @@
-import React, { useContext, useEffect } from 'react';
+import React from 'react';
 import styled from 'styled-components';
 import { useQuery } from '@apollo/react-hooks';
 import GET_TICKETS from './query';
-import { CityContext } from './../../stores';
+import TicketItem from './TicketItem';
 
 const TicketList = _ => {
-  const { cityList, setCityList } = useContext(CityContext);
   const { loading, error, data } = useQuery(GET_TICKETS);
 
   if (loading) return <div>loading...</div>;
-  if (error) return <div>error</div>;
+  //   if (error) return <div>error</div>;
 
-  return (
-    <div>
-      <div></div>
-    </div>
-  );
+  //   const Tickets = data.tickets.map(({ name, airport }) => <TicketItem key={airport} name={name} />);
+
+  const Tickets = [
+    { _id: '1', tripType: 'OW', cities: [{ name: '인천', airport: 'INC' }] },
+    { _id: '2', tripType: 'RT', cities: [{ name: '바르셀로나', airport: 'BC' }] },
+    {
+      _id: '3',
+      tripType: 'MD',
+      cities: [
+        { name: '마드리드', airport: 'MD' },
+        { name: '뉴욕', airport: 'NY' },
+      ],
+    },
+    {
+      _id: '4',
+      tripType: 'MD',
+      cities: [
+        {
+          name: '마드리드',
+          airport: 'MD',
+        },
+        {
+          name: '인천',
+          airport: 'INC',
+        },
+        {
+          name: '바르셀로나',
+          airport: 'BC',
+        },
+      ],
+    },
+  ].map(({ _id, tripType, cities }) => (
+    <TicketItem key={_id} tripType={tripType} cities={cities} />
+  ));
+
+  return <div>{Tickets}</div>;
 };
 
 export default TicketList;
