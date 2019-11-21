e1a1403530c628612b1ca6cb80ea4b6e1b7622fb
Eunwoo <dmsdn960@gmail.com>
2019-11-21 21:49:53 +0900

    BUG: props 설정 관련 오류 수정

---
+++ client/src/components/Main/AttractionTab/AttractionList/HotelItem.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const HotelItem = _ => {
+  return (
+    <div>
+      <div></div>
+    </div>
+  );
+};
+
+export default HotelItem;
--- client/src/components/Main/AttractionTab/AttractionList/index.jsx
+++ client/src/components/Main/AttractionTab/AttractionList/index.jsx
@@ -1,13 +1,15 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useContext } from 'react';
 import { useLazyQuery } from '@apollo/react-hooks';
-import { GET_RECOMMAND } from './query';
+import PropTypes from 'prop-types';
+import GET_RECOMMAND from './query';
 import { CityContext } from '../../../../stores';
+import HotelItem from './HotelItem';
 
-const AttractionList = _ => {
-  const [getRecommand, { called, loading, error, data }] = useLazyQuery(GET_RECOMMAND, {
-    variables: { airport },
-  });
+const AttractionList = ({ currentTab }) => {
   const { city } = useContext(CityContext);
+  const [getRecommand, { called, loading, error, data }] = useLazyQuery(GET_RECOMMAND, {
+    variables: { airport: city.airport },
+  });
 
   useEffect(() => {
     if (city) {
@@ -16,13 +18,50 @@ const AttractionList = _ => {
   }, [city]);
 
   if (called && loading) return <p>Loading ...</p>;
-  if (error) return <p>error</p>;
+  //   if (error) return <p>error</p>;
 
+  const hotels = [
+    {
+      name: '호텔이름1',
+      price: 500,
+      rate: 9.9,
+      image: 'url1',
+      _id: '1',
+    },
+    {
+      name: '호텔이름2',
+      price: 500,
+      rate: 9.9,
+      image: 'url2',
+      _id: '2',
+    },
+    {
+      name: '호텔이름3',
+      price: 500,
+      rate: 9.9,
+      image: 'url3',
+      _id: '3',
+    },
+    {
+      name: '호텔이름4',
+      price: 500,
+      rate: 9.9,
+      image: 'url4',
+      _id: '4',
+    },
+  ].map(({ name, price, rate, image, _id }) => (
+    <HotelItem key={_id} name={name} price={price} rate={rate} image={image} />
+  ));
   return (
     <div>
-      <div></div>
+      {currentTab === 'hotel' && hotels}
+      {/* {currentTab === 'tour' && tours} */}
     </div>
   );
+};
+
+AttractionList.propTypes = {
+  currentTab: PropTypes.string.isRequired,
 };
 
 export default AttractionList;
