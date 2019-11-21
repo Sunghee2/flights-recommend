2800a871aab37f0a052cb03b2a7c09b3ba200168
Eunwoo <dmsdn960@gmail.com>
2019-11-22 00:41:46 +0900

    ENH: 투어 선택 시 컨텍스트에 선택된 투어 전달 #23

--- client/src/components/Main/AttractionTab/AttractionList/TourItem.jsx
+++ client/src/components/Main/AttractionTab/AttractionList/TourItem.jsx
@@ -1,17 +1,22 @@
-import React from 'react';
+import React, { useContext, useCallback } from 'react';
 import styles from 'styled-components';
 import PropTypes from 'prop-types';
+import { TotalContext } from './../../../../stores';
 
 const TourItem = ({ _id, name, price, rank, image }) => {
+  const { setTour } = useContext(TotalContext);
   const Thumbnail = styles.div`
         background-image: url(${image});
         color: white;
         background-repeat: no-repeat;
         background-size: contain;
     `;
+  const selectTour = useCallback(() => {
+    setTour({ _id, name, price, rate, image });
+  });
 
   return (
-    <Thumbnail>
+    <Thumbnail onClick={selectTour}>
       <div>{name}</div>
       <div>{price}</div>
       <div>{rank}</div>
